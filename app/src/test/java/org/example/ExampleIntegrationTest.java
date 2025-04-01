f6274e2a60b519dca17e2a337a4dd861706c6d46
Eric Chang <e850506@gmail.com>
2025-04-01 09:50:58 +0800

    feat: add Embedded Flink example

---
+++ app/src/test/java/org/example/ExampleIntegrationTest.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.test.util.MiniClusterWithClientResource;
+import org.junit.ClassRule;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExampleIntegrationTest {
+
+    @ClassRule
+    public static MiniClusterWithClientResource flinkCluster =
+            new MiniClusterWithClientResource(
+                    new MiniClusterResourceConfiguration.Builder()
+                            .setNumberSlotsPerTaskManager(2)
+                            .setNumberTaskManagers(1)
+                            .build());
+
+    @Test
+    public void testIncrementPipeline() throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // configure your test environment
+        env.setParallelism(2);
+
+        // values are collected in a static variable
+        CollectSink.values.clear();
+
+        // create a stream of custom elements and apply transformations
+        env.fromElements(1L, 21L, 22L)
+                .map(new IncrementMapFunction())
+                .addSink(new CollectSink());
+
+        // execute
+        env.execute();
+
+        List<Long> values = CollectSink.values;
+
+        // verify your results
+        Assertions.assertTrue(CollectSink.values.containsAll(List.of(2L, 22L, 23L)));
+    }
+
+    // create a testing sink
+    private static class CollectSink implements SinkFunction<Long> {
+
+        // must be static
+        public static final List<Long> values = Collections.synchronizedList(new ArrayList<>());
+
+        @Override
+        public void invoke(Long value, SinkFunction.Context context) throws Exception {
+            values.add(value);
+        }
+    }
+}
