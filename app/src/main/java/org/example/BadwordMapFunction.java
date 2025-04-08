94f2820bed285156e99b3ca85d6a58ada3ccbe25
Eric Chang <e850506@gmail.com>
2025-04-08 10:33:11 +0800

    wip: split string to words: split to List<BadwordEntry>

--- app/src/main/java/org/example/BadwordMapFunction.java
+++ app/src/main/java/org/example/BadwordMapFunction.java
@@ -4,29 +4,71 @@ import com.google.common.collect.ImmutableMap;
 import org.apache.flink.api.common.functions.MapFunction;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class BadwordMapFunction implements MapFunction<String, List<BadwordEntry>> {
     private static Map<String, Boolean> badWords = ImmutableMap.of("fuck", true, "shit", true, "damn", true);
 
-    public static String[] split(String record) {
-        // split by , or white space
-        return record.split("[, ]+");
+//    public static String[] split(String record) {
+//        // split by , or white space
+//        return record.split("[, ]+");
+//    }
+
+    public static List<BadwordEntry> split(String record) {
+        ArrayList<BadwordEntry> entries = new ArrayList<BadwordEntry>();
+        Map<String, Boolean> del = Map.of(",", true, " ", true);
+        int len = record.length();
+        int l = 0;
+        int r = l;
+        char[] chars = record.toCharArray();
+        while (l < len) {
+//            r = l;
+            // r == l
+            while (r < len && !del.containsKey(String.valueOf(chars[r]))) {
+                r++;
+            }
+            // word: [l, r]
+            String word = record.substring(l, r);
+            System.out.println("word: " + word);
+            if (badWords.containsKey(word)) {
+                entries.add(new BadwordEntry(List.of(word), new Position(l, r - 1)));
+            }
+            // skip delimiters
+            while (r < len && del.containsKey(String.valueOf(chars[r]))) {
+                r++;
+            }
+            l = r;
+        }
+
+        return entries;
     }
 
     @Override
     public List<BadwordEntry> map(String record) throws Exception {
-        List<BadwordEntry> entries = new ArrayList<>();
-        entries.add(new BadwordEntry(
-                new ArrayList<>(List.of("fuck")),  // Use ArrayList instead of List.of()
-                new Position(0, 3)
-        ));
-        entries.add(new BadwordEntry(
-                new ArrayList<>(List.of("damn")),
-                new Position(10, 13)
-        ));
-        return entries;
+//        List<BadwordEntry> entries = new ArrayList<>();
+        return split(record);
+//        Arrays.stream(split(record)).forEach(word -> {
+//            if (badWords.containsKey(word)) {
+//                entries.add(new BadwordEntry(
+//                        new ArrayList<>(List.of(word)),  // Use ArrayList instead of List.of()
+//                        new Position(0, 3)
+//                ));
+//            };
+//        });
+//        return entries;
+//
+//        entries.add(new BadwordEntry(
+//                new ArrayList<>(List.of("fuck")),  // Use ArrayList instead of List.of()
+//                new Position(0, 3)
+//        ));
+//        entries.add(new BadwordEntry(
+//                new ArrayList<>(List.of("damn")),
+//                new Position(10, 13)
+//        ));
+//        return entries;
     }
 }
 
--- app/src/test/java/org/example/BadwordMapFunctionTest.java
+++ app/src/test/java/org/example/BadwordMapFunctionTest.java
@@ -1,11 +1,10 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,17 +12,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class BadwordMapFunctionTest {
     public static Stream<Arguments> provideTestCases() {
         return Stream.of(
-                Arguments.of("hello world", new String[]{"hello", "world"}),
-                Arguments.of("hello,world", new String[]{"hello", "world"}),
-                Arguments.of("hello , world", new String[]{"hello", "world"}),
-                Arguments.of("hello ,  world", new String[]{"hello", "world"})
+                Arguments.of("fuck this damn project", List.of(
+                        new BadwordEntry(List.of("fuck"), new Position(0, 3)),
+                        new BadwordEntry(List.of("damn"), new Position(10, 13))
+                ))
         );
     }
 
     @ParameterizedTest
     @MethodSource("provideTestCases")
-    public void testSplit(String record, String[] words) {
-        BadwordMapFunction fn = new BadwordMapFunction();
-        assertArrayEquals(words, BadwordMapFunction.split(record));
+    public void testSplit(String record, List<BadwordEntry> entries) {
+        assertEquals(entries, BadwordMapFunction.split(record));
     }
 }
